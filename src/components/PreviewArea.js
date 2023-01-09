1ece7871a982b09b79bd9c8f105362b70debde93
Carmen Lopez <carmenbaez2000@gmail.com>
2023-01-09 16:00:29 +0100

    Summary
    1) Added tabs for:
    
    - Preview components
    - Code
    
    2) Logic to download code as .html

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,8 +1,40 @@
+import React, { useState } from "react";
+
 function PreviewArea() {
+  const [htmlCode, sethtmlCode] = useState("The code will be here");
+  const [showTab, setShowTab] = useState(true);
+
+  async function saveToFile() {
+    let myBlob = new Blob([htmlCode], { type: "text/document" });
+    let url = URL.createObjectURL(myBlob);
+    let link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", "index.html");
+    link.click();
+  }
+
   return (
-    <section className="previewArea">
-      <h3>Preview Area</h3>
-    </section>
+    <div>
+      <span>
+        <button className="btnShowTab" onClick={() => setShowTab(true)}>
+          PREVIEW
+        </button>
+        <button className="btnShowTab" onClick={() => setShowTab(false)}>
+          CODE
+        </button>
+      </span>
+      <span className="overlapArea">
+        <section
+          className={showTab ? "previewArea showTab" : "previewArea hideTab"}
+        ></section>
+        <section className={!showTab ? "codeArea showTab" : "codeArea hideTab"}>
+          <button onClick={saveToFile}>Save</button>
+          <pre id="generatedCode" onChange={(e) => sethtmlCode(e.target.value)}>
+            The code will be here
+          </pre>
+        </section>
+      </span>
+    </div>
   );
 }
 

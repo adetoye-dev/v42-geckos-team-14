e86ad7e5400ab3c7b5368b12cfc117baf13ac018
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-12 12:32:50 +0100

    Add resizers to test component

--- src/components/ComponentList.js
+++ src/components/ComponentList.js
@@ -1,11 +1,18 @@
+import useResizer from "./useResizer";
+import "./Resizer.css";
+
 function ComponentList() {
-    return (
-      <section>
-        <div className="c1">Component 1</div>
-        <div className="c2">Component 2</div>
-        <div className="c3">Component 3</div>
-      </section>
-    );
+  const [ref, resizers] = useResizer();
+  return (
+    <section>
+      <div className="c1">Component 1</div>
+      <div ref={ref} className="c2">
+        {" "}
+        {resizers}Component 2
+      </div>
+      <div className="c3">Component 3</div>
+    </section>
+  );
 }
 
 export default ComponentList;

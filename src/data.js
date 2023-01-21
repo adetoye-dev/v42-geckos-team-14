568d1b2d2102c6e1d74c8869455afab316ed512e
carmen <58992828+cgbl-90@users.noreply.github.com>
2023-01-21 19:16:22 +0000

    summary

--- src/data.js
+++ src/data.js
@@ -5,21 +5,37 @@ const data = [
     name: "Bullet list",
     tag: "ul",
     appliedOnlyOnce: false,
+    canBeEdited: true,
   },
   {
     name: "Button",
     tag: "button",
     appliedOnlyOnce: false,
+    canBeEdited: true,
   },
   {
     name: "[Social media] link",
     tag: "a",
     appliedOnlyOnce: false,
+    canBeEdited: true,
   },
   {
     name: "Header with logo",
     tag: "header",
     appliedOnlyOnce: true,
+    canBeEdited: true,
+  },
+  {
+    name: "Divide line",
+    tag: "hr",
+    appliedOnlyOnce: false,
+    canBeEdited: false,
+  },
+  {
+    name: "Go up arrow",
+    tag: "span",
+    appliedOnlyOnce: false,
+    canBeEdited: false,
   },
 ];
 

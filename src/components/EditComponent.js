86a92a7992387c32e0ab3af57dede9dacfe0ec0b
carmen <58992828+cgbl-90@users.noreply.github.com>
2023-01-26 19:42:30 +0000

    Summary
    Edited array values + added EditComponent.js

---
+++ src/components/EditComponent.js
@@ -0,0 +1,3 @@
+// After we render the components, we need to be able to edit the text, images as necessary
+// This component edits the data and save it in htmlCode var
+// components var should not be edited

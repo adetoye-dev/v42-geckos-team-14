64c4cc2b12f15a950a8f529a4988d994b11625af
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-06 11:35:47 +0100

    Move context provider to index.js Move context provider to index.js so that it can be accessible in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ContextProvider } from "./components/Context";
 import PreviewArea from "./components/PreviewArea";
 import ComponentList from "./components/ComponentList";
 import NavbarComp from "./components/NavbarComp";
@@ -45,13 +44,13 @@ library.add(
 
 function App() {
   return (
-    <ContextProvider>
+    <>
       <NavbarComp />
       <div className="flex">
         <PreviewArea />
         <ComponentList />
       </div>
-    </ContextProvider>
+    </>
   );
 }
 
